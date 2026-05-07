19de6b403d7a14e1a72fe5f64fa9f2a9ac01043c
Jakub Pietrzak <zcfrost@gmail.com>
2026-05-07 10:11:27 +0200

    Auto sync editor changes 2026-05-07 10:11:26

--- data.js
+++ data.js
@@ -37,7 +37,7 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-a-1",
                                  "author":  "Szymon",
-                                 "likes":  303,
+                                 "likes":  20,
                                  "text":  "zdrowy środek. Słodycze są szkodliwe ale można czasem pozwolić sobie na nie.",
                                  "side":  "a",
                                  "replies":  [
@@ -47,7 +47,7 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-b-1",
                                  "author":  "Adam",
-                                 "likes":  332,
+                                 "likes":  33,
                                  "text":  "jak wiesz ze coś Cię truje to jest jak samobójstwo ale rozłożone w czasie.",
                                  "side":  "b",
                                  "replies":  [
@@ -60,7 +60,7 @@ window.DIET_WARS = [
         "id":  "nowy-temat-1777918778601",
         "title":  "Naturalne wędzenie czy „malowanie” aromatem dymu wędzarniczego?",
         "kicker":  "Czy aromat dymu jest bezpieczniejszą technologią, czy tylko przemysłową imitacją tradycji?",
-        "badge":  "nowe badania",
+        "badge":  "mało znane fakty",
         "category":  "Nowe",
         "sideA":  "Roztwór dymu wędzarniczego",
         "sideB":  "Naturalne wędzenie",
@@ -113,12 +113,12 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-a-1",
                                  "author":  "AP",
-                                 "likes":  314,
+                                 "likes":  36,
                                  "text":  "nie jem mięsa więc nie mam problemu z wyoborem",
                                  "side":  "a",
-                                 "replies":  {
+                                 "replies":  [
 
-                                             }
+                                             ]
                              },
                              {
                                  "id":  "seed-a-2",
@@ -126,19 +126,19 @@ window.DIET_WARS = [
                                  "likes":  22,
                                  "text":  "To po co zabierasz głos?",
                                  "side":  "a",
-                                 "replies":  {
+                                 "replies":  [
 
-                                             }
+                                             ]
                              },
                              {
                                  "id":  "seed-b-1",
                                  "author":  "Marek",
-                                 "likes":  370,
+                                 "likes":  45,
                                  "text":  "mam większe zaufanie do intuicji i wieloletniego doświadczenia niż jakiś dziwnych ideologicznych instytucji.",
                                  "side":  "b",
-                                 "replies":  {
+                                 "replies":  [
 
-                                             }
+                                             ]
                              }
                          ]
     },
@@ -199,14 +199,14 @@ window.DIET_WARS = [
                                  "id":  "seed-a-1",
                                  "side":  "a",
                                  "author":  "Marek",
-                                 "likes":  294,
+                                 "likes":  21,
                                  "text":  "Ludzie od pokoleń jedzą i jedli mięso więc to tylko kolejny wydziwianie dietetyków. brednie"
                              },
                              {
                                  "id":  "seed-b-1",
                                  "side":  "b",
                                  "author":  "Anna",
-                                 "likes":  354,
+                                 "likes":  32,
                                  "text":  "Jak wam nie żal mordowania zwierząt. Bierze mnie na wymioty jak widzę mięso. Zwierzęta są traktowane bardzo źle."
                              }
                          ]
@@ -268,14 +268,14 @@ window.DIET_WARS = [
                                  "id":  "seed-a-1",
                                  "side":  "a",
                                  "author":  "Tomasz",
-                                 "likes":  295,
+                                 "likes":  11,
                                  "text":  "jak zwiększam ilosć tłuszczu w diecie, mam mniejsze wyrzuty insuliny, wiec czuję się stabilnie i dobrze cały dzień. Poza tym nawet poprawił mi się sen - śpię idealnie i tyle samo jak z zegarkiem."
                              },
                              {
                                  "id":  "seed-b-1",
                                  "side":  "b",
                                  "author":  "Anonim",
-                                 "likes":  352,
+                                 "likes":  22,
                                  "text":  "kolejne brednie, a potem się dziwić, ze ludzie mają zawały i inne problemy. Moze jeszcze jak ktoś je avocado to lepiej, ale tłuszcze nasycone, masło, boczek to podnosi ryzyko miażsżycy, zwiększa zły cholesterol."
                              }
                          ]
@@ -336,7 +336,7 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-a-1",
                                  "author":  "Anna",
-                                 "likes":  320,
+                                 "likes":  23,
                                  "text":  "Kiedyś było tylko mleko krowie, piło się je codzinnie, nawet w szkole i nikt nie miał nietoleracji czy problemów z tym związanych.",
                                  "side":  "a",
                                  "replies":  null
@@ -344,7 +344,7 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-b-1",
                                  "author":  "Małgośka",
-                                 "likes":  339,
+                                 "likes":  34,
                                  "text":  "mleko to także tłuszcze nasycone i może obciążać układ krążenia.  Mleko = mocne kości to po prostu mit.",
                                  "side":  "b",
                                  "replies":  null
@@ -408,14 +408,14 @@ window.DIET_WARS = [
                                  "id":  "seed-a-1",
                                  "side":  "a",
                                  "author":  "Anonim",
-                                 "likes":  336,
+                                 "likes":  44,
                                  "text":  "to najlepszej jakości białko, świetnie przyswajalne. Poza tym jajka to też cholina i wzmacnia pamięć, koncentrację i poprawia działanie układu nerwowego."
                              },
                              {
                                  "id":  "seed-b-1",
                                  "side":  "b",
                                  "author":  "Anonim",
-                                 "likes":  382,
+                                 "likes":  64,
                                  "text":  "jajka to źródło cholesterolu. Wystarczy badać poziom LDL i zmienić dietę, od razu widać zmianę."
                              }
                          ]
@@ -477,14 +477,14 @@ window.DIET_WARS = [
                                  "id":  "seed-a-1",
                                  "side":  "a",
                                  "author":  "Anonimous",
-                                 "likes":  369,
+                                 "likes":  59,
                                  "text":  "zdecydowanie za, schudłem prawie 40kg, mniej podjadam i nie muszę liczyć kalorii."
                              },
                              {
                                  "id":  "seed-b-1",
                                  "side":  "b",
                                  "author":  "Krystyna",
-                                 "likes":  392,
+                                 "likes":  48,
                                  "text":  "na mnie działa, mam napady głodu, a rano źle się czuje jak nie zjem."
                              }
                          ]
@@ -545,7 +545,7 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-a-1",
                                  "author":  "Anonim",
-                                 "likes":  314,
+                                 "likes":  11,
                                  "text":  "sól zwiększa zatrzymanie wody w organizmie, podnosi ciśnienia, niszczy nerki. To chyba wystarczająco przebadane. Tylko jak ktoś chce być oryginalny jak wspominani dietetycy to mówi głupoty przeciwe. Wg WHO nadmiar soli jest gorszy niż niedobór.",
                                  "side":  "a",
                                  "replies":  null
@@ -553,7 +553,7 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-b-1",
                                  "author":  "RPp",
-                                 "likes":  350,
+                                 "likes":  15,
                                  "text":  "sól pomaga mi bo mam niskie ciśnienie. Poza tym WHO to żaden autorytet. Bracia Rodzeń udowodnili że bardzo jest niezbędna sól dla zdrowia.",
                                  "side":  "b",
                                  "replies":  null
@@ -616,7 +616,7 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-a-1",
                                  "author":  "Łukasz",
-                                 "likes":  366,
+                                 "likes":  54,
                                  "text":  "Zdrowy tryb życia, trochę ruchu i niewielki deficyt załatwia sprawę. Jak chcesz dobrze wyglądac to musisz włożyć trochę wysiłku. Za to potem nie wstyd wyjść na plażę :)",
                                  "side":  "a",
                                  "replies":  null
@@ -624,7 +624,7 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-b-1",
                                  "author":  "Łukasz",
-                                 "likes":  321,
+                                 "likes":  11,
                                  "text":  "szczerze mówiąc to musiałbym cały czas chodzi głodny żeby nie przytyc a co dopiero schudnąć. Jak masz siedzący tryb życia to chyba jest się skazanym na nadwagę.",
                                  "side":  "b",
                                  "replies":  null
@@ -687,7 +687,7 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-a-1",
                                  "author":  "Anonim",
-                                 "likes":  318,
+                                 "likes":  49,
                                  "text":  "To samo co w dyskusji o tłuszczach - jednemu służy innemu nie. Ja czuję się świetnie.",
                                  "side":  "a",
                                  "replies":  null
@@ -695,7 +695,7 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-b-1",
                                  "author":  "Anonim",
-                                 "likes":  406,
+                                 "likes":  92,
                                  "text":  "tak? a ja znam przynajmniej dwie osoby, które super schudły jedząc boczek a w potem niestety młodo zmarły. Oczywiscie niekoniecznie z powodu diety, moze to tylko zbiego okoliczności.",
                                  "side":  "b",
                                  "replies":  null
@@ -766,7 +766,7 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-a-2",
                                  "author":  "Anonim",
-                                 "likes":  391,
+                                 "likes":  71,
                                  "text":  "a co ma religia do autofagii??",
                                  "side":  "a",
                                  "replies":  null
@@ -774,7 +774,7 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-b-1",
                                  "author":  "Anonim",
-                                 "likes":  330,
+                                 "likes":  11,
                                  "text":  "Łatwo przekroczyć granicę i wejsć w niedożywienie, taka głodówka mi nigdy nie służyła bo się odbijała na masie mięśniowej. Poza tym to kolejne wydziwianie i nie zbadane porzadnie zjawisko.  jestem na nie.",
                                  "side":  "b",
                                  "replies":  null
@@ -837,7 +837,7 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-a-1",
                                  "author":  "Andrzej",
-                                 "likes":  378,
+                                 "likes":  76,
                                  "text":  "stosuję od lat, czuje się bardzo dobrze - zdecydowanie polecam!",
                                  "side":  "a",
                                  "replies":  null
@@ -853,7 +853,7 @@ window.DIET_WARS = [
                              {
                                  "id":  "seed-b-1",
                                  "author":  "AN79",
-                                 "likes":  357,
+                                 "likes":  51,
                                  "text":  "Typowa dieta z bardzo mocnym ograniczeniem kalorii. Niby się chudnie a potem mocny efekt jojo. Nie lubię i nie służy mi.",
                                  "side":  "b",
                                  "replies":  null
